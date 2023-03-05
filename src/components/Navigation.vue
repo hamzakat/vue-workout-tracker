2bd7dba6e7a4cb1c239c3c7602f4b791c80a105c
Hamza <superusermhk@gmail.com>
2023-03-05 15:37:04 +0300

    Add stores and make the navbar reactive with the user state

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -14,25 +14,33 @@
         <router-link class="cursor-pointer" :to="{ name: '' }"
           >Create</router-link
         >
-        <router-link class="cursor-pointer" :to="{ name: 'Login' }"
+        <router-link v-if="!user" class="cursor-pointer" :to="{ name: 'Login' }"
           >Login</router-link
         >
-        <li class="cursor-pointer">Logout</li>
+        <li @click="logout" v-if="user" class="cursor-pointer">Logout</li>
       </ul>
     </nav>
   </header>
 </template>
 
 <script>
+import { useRouter } from "vue-router";
+import { supabase } from "../supabase/init";
+import store from "../store";
+import { computed } from "vue";
 export default {
   setup() {
     // Get user from store
-
+    const user = computed(() => store.state.user);
     // Setup ref to router
+    const router = useRouter();
 
     // Logout function
-
-    return {};
+    const logout = async () => {
+      await supabase.auth.signOut();
+      router.push({ name: "Home" });
+    };
+    return { logout, user };
   },
 };
 </script>
