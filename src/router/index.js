5c29ebcc5d22422ca04e2c4817c2ebd747a23e8b
Hamza <superusermhk@gmail.com>
2023-03-06 19:48:33 +0300

    Add route guard for auth routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,26 +4,44 @@ import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import Create from "../views/Create.vue";
 import ViewWorkout from "../views/ViewWorkout.vue";
+import { supabase } from "../supabase/init";
+
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      title: "Home",
+      auth: false,
+    },
   },
   {
     path: "/login",
     name: "Login",
     component: Login,
+    meta: {
+      title: "Login",
+      auth: false,
+    },
   },
   {
     path: "/register",
     name: "Register",
     component: Register,
+    meta: {
+      title: "Register",
+      auth: false,
+    },
   },
   {
     path: "/create",
     name: "Create",
     component: Create,
+    meta: {
+      title: "Creat",
+      auth: true,
+    },
   },
   {
     path: "/view-workout/:workoutId",
@@ -42,7 +60,25 @@ const router = createRouter({
 });
 
 // Change document titles
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | Workout Tracker`;
+  next();
+});
 
 // Route guard for auth routes
+router.beforeEach((to, from, next) => {
+  const user = supabase.auth.user();
+  // check if the distination requires auth
+  if (to.matched.some((res) => res.meta.auth)) {
+    // check if the user is logged in
+    if (user) {
+      next(); // continue safely
+      return;
+    } else {
+      // redirect to Login page
+      next({ name: "Login" });
+    }
+  }
+});
 
 export default router;
